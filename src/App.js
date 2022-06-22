cdc1e31d98dd15dc8f536f226fdbc5e0eae3a180
Glen E. Martinez <glene.martinez@MacBook-Pro-de-Glen.local>
2022-06-22 07:04:04 -0300

    webpack inline style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
+const divStyle = {
+  color: 'blue',
+  textAlign: 'center'
+}
 const App = () => {
   return(
-    <>
+    <div style = {divStyle}>
       <p>hola quetal</p>
       <p>webPack</p>
       <button onClick={()=>console.log('click')}>click</button>
-    </>
+    </div>
   )
 }
 export default App
